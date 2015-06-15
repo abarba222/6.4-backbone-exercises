9429bdf3d7304373763f5b8d2674cb83cce2d0dc
Anthony Barba <abarba222@gmail.com>
2015-06-15 15:28:14 -0400

    how bout C

--- scripts/router.js
+++ scripts/router.js
@@ -1,1 +1,40 @@
+import IndexView from './views/index';
+import PostShowView from './views/show';
+import {PostCollection} from './models/post';
 
+
+export default Backbone.Router.extend ({
+  routes: {
+    '': 'index',
+    ':id': 'show'
+  },
+
+  intialize: function(){
+
+    this.posts = new PostCollection();
+    this.fetchPostsPromise = this.posts.fetch();
+
+    this.indexView = new indexView({collection: this.posts});
+    $('#sidebar').html(this.indexView.el);
+
+},
+
+  index: function() {
+
+  },
+
+  show: function(id) {
+    this.fetchPostsPromise.then(function() {
+      var post = this.posts.get(id);
+      this.showView(new PostShowView({model: post}));
+    }.bind(this));
+  },
+
+  showView: function(view) {
+    if(this.currentView)this.currentView.remove();
+    this.currentView = view;
+    $('#app-container').html(view.el);
+    return view;
+  }
+
+});
